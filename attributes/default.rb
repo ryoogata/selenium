97b496e89b1bd332bb4a23c46167467f0e3b7ef5
ryoogata <ryo.ogata@gmail.com>
2013-11-23 23:21:41 +0900

    selenium のバージョンを指定できるように修正

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,2 @@
-default["selenium"]["node"]["_REMOTE"] = "http://ec2-184-169-207-220.us-west-1.compute.amazonaws.com:4444/grid/register"
+default["selenium"]["node"]["_REMOTE"] = "http://localhost:4444/grid/register"
+default["selenium"]["_VERSION"] = "2.37.0"
